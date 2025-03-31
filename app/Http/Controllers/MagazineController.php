0b7963648e5bd22efb0466d836e5d41a9aea7fca
Nikita <nik45.nik@mail.ru>
2025-03-31 21:42:39 +0300

    first commit

--- app/Http/Controllers/MagazineController.php
+++ app/Http/Controllers/MagazineController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\MagazineVisits;
 use App\Models\Training;
 use App\Models\traning_group;
+use App\Models\Users;
+use App\Models\UsersDocument;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\SimpleType\Jc;
 use Rap2hpoutre\FastExcel\Facades\FastExcel;
 use function PHPUnit\Framework\isEmpty;
 
@@ -137,11 +140,13 @@ class MagazineController extends Controller
         //dump($users);
 
         $doc = new TemplateProcessor(storage_path('app/public/doc/proba.docx'));
+        // Дни недели в числовом формате (1 — понедельник, 7 — воскресенье)
+        $weekdays = range(1, 7);
+        shuffle($weekdays);
+        $randomWeekdays = array_slice($weekdays, 0, 3);
 
-        $day = self::getRandomWeekdaysDates(2024, 9);
-        $count = count($day['days']);
-
-        // Создаем таблицу, которую нужно вставить в шаблон
+        //____________________________________________
+        // Создаем таблицу, общие сведения
         $phpWord = new PhpWord();
         $section = $phpWord->addSection();
         $tableUser = $section->addTable([
@@ -149,6 +154,12 @@ class MagazineController extends Controller
             'borderColor' => '000000', // Черный цвет границы
             'cellMargin' => 50, // Отступ внутри ячеек
         ]);
+
+        $tableStyle = [
+            'borderSize' => 6,
+            'borderColor' => '000000',
+            'cellMargin' => 50,
+        ];
 
         // Добавляем заголовки с границами
         $tableUser->addRow();
@@ -161,14 +172,112 @@ class MagazineController extends Controller
 
         // Добавляем данные пользователей с границами
         foreach ($users as $key => $user) {
+            $fullName = $user['user'];
+            if (preg_match('/^\S+/', $fullName, $matches)) {
+                $lastName = $matches[0]; // Фамилия
+            }
+            $user_id = Users::query()->where('surname', $lastName)->value('id');
+            $userPhone = UsersDocument::query()->where('user_id', $user_id)->value('representative_phone') ?? ' ';
             $tableUser->addRow();
             $tableUser->addCell(200, ['borderSize' => 6, 'borderColor' => '000000'])->addText($key + 1);
-            $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText($user['user']);
+            $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText(trim($user['user']));
             $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText($user['br'] ?? '');
             $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText($user['start'] ?? '');
             $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText($user['end'] ?? '');
-            $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText('3454353453453');
+            $tableUser->addCell(2000, ['borderSize' => 6, 'borderColor' => '000000'])->addText($userPhone);
         }
+
+        //_____________________________________________________
+        //создаем таблицу расписаний
+        $tableSchedule = $section->addTable($tableStyle);
+
+        // Заголовки колонок
+        $headerRow = ['', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+        $tableSchedule->addRow();
+        foreach ($headerRow as $header) {
+            $tableSchedule->addCell(1500, ['valign' => 'center'])->addText($header, ['bold' => true], ['alignment' => Jc::CENTER]);
+        }
+
+        // Месяцы
+        $months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+        // Заполняем таблицу
+        foreach ($months as $month) {
+            $tableSchedule->addRow();
+            $tableSchedule->addCell(1500, ['valign' => 'center'])->addText($month, ['bold' => true]);
+
+            // Заполняем ячейки пустыми значениями
+            for ($i = 1; $i <= 7; $i++) {
+                $cell = $tableSchedule->addCell(1500, ['valign' => 'center']);
+
+                // Добавляем расписание только для сентября–декабря (как на картинке)
+                if (in_array($month, ['Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']) && in_array($i, $randomWeekdays)) {
+                    $cell->addText("17:00-18:00\n18:15-19:15", ['size' => 10], ['alignment' => Jc::CENTER]);
+                }
+            }
+        }
+
+        $daySep = self::getRandomWeekdaysDates(2024, 9, $randomWeekdays);
+        $dayOct = self::getRandomWeekdaysDates(2024, 10, $randomWeekdays);
+        $dayNov = self::getRandomWeekdaysDates(2024, 11, $randomWeekdays);
+        $dayDes = self::getRandomWeekdaysDates(2024, 12, $randomWeekdays);
+
+        $tableVisitSept = self::createTableVisitDoc('Сентябрь', $daySep, $users);
+        $tableVisitOct = self::createTableVisitDoc('Октябрь', $dayOct, $users);
+        $tableVisitNov = self::createTableVisitDoc('Ноябрь', $dayNov, $users);
+        $tableVisitDes = self::createTableVisitDoc('Декабрь', $dayDes, $users);
+
+
+        //_____________________________________________________
+        // Заменяем метку ${tegs} в шаблоне на HTML-код таблицы
+        $doc->setComplexBlock('user_table', $tableUser);
+        $doc->setComplexBlock('table_schedule', $tableSchedule);
+        $doc->setComplexBlock('user_visit_sep', $tableVisitSept);
+        $doc->setComplexBlock('user_visit_oct', $tableVisitOct);
+        $doc->setComplexBlock('user_visit_nov', $tableVisitNov);
+        $doc->setComplexBlock('user_visit_des', $tableVisitDes);
+        $doc->setValue('coach', $coach);
+        $doc->setValue('sport', $sport);
+
+        // Сохраняем итоговый документ
+        $filename = 'users_table_from_template.docx';
+        $path = storage_path('app/public/success/' . $filename);
+        $doc->saveAs($path);
+
+        // Отправляем файл на скачивание
+        return response()->download($path)->deleteFileAfterSend(true);
+    }
+
+    public function getRandomWeekdaysDates($year, $month, $daysWeek)
+    {
+
+
+        $dates = [];
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+        for ($day = 1; $day <= $daysInMonth; $day++) {
+            $dateString = sprintf('%04d-%02d-%02d', $year, $month, $day);
+            $weekday = date('N', strtotime($dateString));
+
+            if (in_array($weekday, $daysWeek)) {
+                $dates[$weekday][] = date('d', strtotime($dateString));
+            }
+        }
+
+        // Объединяем все даты в один массив и сортируем
+        $allDates = array_merge(...array_values($dates));
+        sort($allDates);
+
+        $data = ['day' => $daysWeek, 'days' => $allDates];
+
+        return $data;
+    }
+
+    public function createTableVisitDoc($month, $day, $users)
+    {
+        $count = count($day['days']);
+        $phpWord = new PhpWord();
+        $section = $phpWord->addSection();
         $tableStyle = [
             'borderSize' => 6,
             'borderColor' => '000000',
@@ -180,7 +289,7 @@ class MagazineController extends Controller
         $tableVisit->addRow();
         $tableVisit->addCell(1000, ['vMerge' => 'restart', 'valign' => 'center'])->addText('№', ['bold' => true]);
         $tableVisit->addCell(4000, ['vMerge' => 'restart', 'valign' => 'center'])->addText('Фамилия, имя', ['bold' => true]);
-        $tableVisit->addCell(8000, ['gridSpan' => $count, 'align' => 'center'])->addText('Сентябрь', ['bold' => true]);
+        $tableVisit->addCell(8000, ['gridSpan' => $count, 'align' => 'center'])->addText($month, ['bold' => true]);
 
         // Вторая строка (даты)
         $tableVisit->addRow();
@@ -199,12 +308,12 @@ class MagazineController extends Controller
             $tableVisit->addCell(4000)->addText($user['user']);
 
             for ($i = 1; $i <= $count; $i++) {
-                $arrayVisit = ['n', '+'];
+                $arrayVisit = ['n', ' '];
                 $key = array_rand($arrayVisit, 1);
                 $value = $arrayVisit[$key]; // Получаем сам текст
                 $tableVisit->addCell(1000)->addText($value); // Добавляем в таблицу
 
-                if ($value == '+') { // Проверяем непосредственно текст
+                if ($value == ' ') { // Проверяем непосредственно текст
                     $columnCounts[$i]++;
                 }
             }
@@ -278,45 +387,6 @@ class MagazineController extends Controller
             $tableVisit->addCell(1000)->addText(''); // Добавляем в таблицу
         }
 
-        // Заменяем метку ${tegs} в шаблоне на HTML-код таблицы
-        $doc->setComplexBlock('user_table', $tableUser);
-        $doc->setComplexBlock('user_visit', $tableVisit);
-        $doc->setValue('coach', $coach);
-        $doc->setValue('sport', $sport);
-
-        // Сохраняем итоговый документ
-        $filename = 'users_table_from_template.docx';
-        $path = storage_path('app/public/success/' . $filename);
-        $doc->saveAs($path);
-
-        // Отправляем файл на скачивание
-        return response()->download($path)->deleteFileAfterSend(true);
-    }
-
-    public function getRandomWeekdaysDates($year, $month) {
-        // Дни недели в числовом формате (1 — понедельник, 7 — воскресенье)
-        $weekdays = range(1, 7);
-        shuffle($weekdays);
-        $randomWeekdays = array_slice($weekdays, 0, 3);
-
-        $dates = [];
-        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-
-        for ($day = 1; $day <= $daysInMonth; $day++) {
-            $dateString = sprintf('%04d-%02d-%02d', $year, $month, $day);
-            $weekday = date('N', strtotime($dateString));
-
-            if (in_array($weekday, $randomWeekdays)) {
-                $dates[$weekday][] = date('d', strtotime($dateString));
-            }
-        }
-
-        // Объединяем все даты в один массив и сортируем
-        $allDates = array_merge(...array_values($dates));
-        sort($allDates);
-
-        $data = ['day' => $randomWeekdays, 'days' => $allDates];
-
-        return $data;
+        return $tableVisit;
     }
 }
